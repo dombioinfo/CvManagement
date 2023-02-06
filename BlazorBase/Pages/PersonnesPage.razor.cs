4891fb2b93361a8634cc69423def10b40200fae5
Dominique Jeannin <djeannin@waya-tech.com>
2023-02-07 00:24:27 +0100

    Mise en place de la datagrid sur les candidats

--- BlazorBase/Pages/PersonnesPage.razor.cs
+++ BlazorBase/Pages/PersonnesPage.razor.cs
@@ -1,6 +1,7 @@
 using BlazorBase.Service;
 using Microsoft.AspNetCore.Components;
 using BlazorBaseModel.Model;
+using Blazorise.DataGrid;
 
 namespace BlazorBase.Pages
 {
@@ -12,11 +13,35 @@ namespace BlazorBase.Pages
         protected PersonneService PersonneService { get; set; } = default!;
         private IEnumerable<PersonneDto> personnes { get; set; } = default!;
         private PersonneDto? selectedPersonne;
+        private int totalPersonnes;
 
         protected override async Task OnInitializedAsync()
         {
             personnes = await PersonneService.GetPersonnesAsync();
             await base.OnInitializedAsync();
+        }
+
+        private async Task OnReadData(DataGridReadDataEventArgs<PersonneDto> e)
+        {
+            if (!e.CancellationToken.IsCancellationRequested)
+            {
+                List<PersonneDto> response = default!;
+
+                // this can be call to anything, in this case we're calling a fictional api
+                //var response = await Http.GetJsonAsync<Employee[]>( $"some-api/employees?page={e.Page}&pageSize={e.PageSize}" );
+                if (e.ReadDataMode is DataGridReadDataMode.Virtualize)
+                    response = (await PersonneService.GetPersonnesAsync()).Skip(e.VirtualizeOffset).Take(e.VirtualizeCount).ToList();
+                else if (e.ReadDataMode is DataGridReadDataMode.Paging)
+                    response = (await PersonneService.GetPersonnesAsync()).Skip((e.Page - 1) * e.PageSize).Take(e.PageSize).ToList();
+                else
+                    throw new Exception("Unhandled ReadDataMode");
+
+                if (!e.CancellationToken.IsCancellationRequested)
+                {
+                    totalPersonnes = (await PersonneService.GetPersonnesAsync()).Count();
+                    personnes = new List<PersonneDto>(response); // an actual data for the current page
+                }
+            }
         }
     }
 }
